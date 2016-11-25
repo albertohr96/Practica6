1e2164267aa6efcd34c1431ca353e66795a99e22
usuario <usuario@10.22.143.48>
2016-11-25 10:38:11 +0100

    Version1

---
+++ src/moneycalculator/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Práctica 6
+ */
+package moneycalculator;
+/**
+ * @Name:Alberto Hernández Rodríguez
+ */
+public class Main {
+    public static void main(String[]args){
+        new MainFrame();
+    }
+}
